b0e16b641b7610686d923ad821f984edaf2786f1
Moussa KANE <princekan9.mk@gmail.com>
2022-10-19 18:02:29 -0700

    I am not going anywhere. You can print that wherever you want to. I am here and I am a Spur for life

---
+++ function_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/*
+ *  funct_putchar - writes a character to stdout
+ * @a The character to print
+ *
+ * Return On success 1.
+ * On error, -1 is returned, and errno is set appropriately
+ */
+
+int funct_putchar(char a)
+{
+	return (write(1, &a, 1));
+}
