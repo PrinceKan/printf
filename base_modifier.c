2eda2b329aa056e377ff479770102c6cce69948d
Moussa KANE <princekan9.mk@gmail.com>
2022-10-21 17:25:31 -0700

    What one has not experienced, one will never understand in print

--- base_modifier.c
+++ base_modifier.c
@@ -40,3 +40,153 @@ int funct_bin(va_list list)
 	free(rev_str);
 	return (len);
 }
+
+/**
+ * funct_oct - Prints the numeric representation of a number in octal base
+ * @list: List of all the arguments passed to the program
+ * Return: Number of symbols printed to stdout
+ */
+int funct_oct(va_list list)
+{
+	unsigned int num;
+	int len;
+	char *octal_rep;
+	char *rev_str;
+
+	num = va_arg(list, unsigned int);
+
+	if (num == 0)
+		return (funct_putchar('0'));
+	if (num < 1)
+		return (-1);
+	len = base_span(num, 8);
+
+	octal_rep = malloc(sizeof(char) * len + 1);
+	if (octal_rep == NULL)
+		return (-1);
+	for (len = 0; num > 0; len++)
+	{
+		octal_rep[len] = (num % 8) + 48;
+		num = num / 8;
+
+	}
+	octal_rep[len] = '\0';
+	rev_str = str_revert(octal_rep);
+	if (rev_str == NULL)
+		return (-1);
+
+	baseScore(rev_str);
+	free(octal_rep);
+	free(rev_str);
+	return (len);
+}
+
+/**
+ * funct_hex - Prints a representation of a decimal number on base16 lowercase
+ * @list: List of the arguments passed to the function
+ * Return: Number of characters printed
+ */
+int funct_hex(va_list list)
+{
+	unsigned int num;
+	int len;
+	int rem_num;
+	char *hex_rep;
+	char *rev_hex;
+
+	num = va_arg(list, unsigned int);
+
+	if (num == 0)
+		return (funct_putchar('0'));
+	if (num < 1)
+		return (-1);
+	len = base_span(num, 16);
+	hex_rep = malloc(sizeof(char) * len + 1);
+	if (hex_rep == NULL)
+		return (-1);
+	for (len = 0; num > 0; len++)
+	{
+		rem_num = num % 16;
+		if (rem_num > 9)
+		{
+			rem_num = Hex_case_checker(rem_num, 'x');
+			hex_rep[len] = rem_num;
+		}
+		else
+			hex_rep[len] = rem_num + 48;
+		num = num / 16;
+	}
+	hex_rep[len] = '\0';
+	rev_hex = str_revert(hex_rep);
+	if (rev_hex == NULL)
+		return (-1);
+	baseScore(rev_hex);
+	free(hex_rep);
+	free(rev_hex);
+	return (len);
+}
+
+
+/**
+ * funct_HEX - Prints a representation of a decimal number on base16 Uppercase
+ * @list: List of the arguments passed to the function
+ * Return: Number of characters printed
+ */
+int funct_HEX(va_list list)
+{
+	unsigned int num;
+	int len;
+	int rem_num;
+	char *hex_rep;
+	char *rev_hex;
+
+	num = va_arg(list, unsigned int);
+
+	if (num == 0)
+		return (funct_putchar('0'));
+	if (num < 1)
+		return (-1);
+	len = base_span(num, 16);
+	hex_rep = malloc(sizeof(char) * len + 1);
+	if (hex_rep == NULL)
+		return (-1);
+	for (len = 0; num > 0; len++)
+	{
+		rem_num = num % 16;
+		if (rem_num > 9)
+		{
+			rem_num = Hex_case_checker(rem_num, 'X');
+			hex_rep[len] = rem_num;
+		}
+		else
+			hex_rep[len] = rem_num + 48;
+		num = num / 16;
+	}
+	hex_rep[len] = '\0';
+	rev_hex = str_revert(hex_rep);
+	if (rev_hex == NULL)
+		return (-1);
+	baseScore(rev_hex);
+	free(hex_rep);
+	free(rev_hex);
+	return (len);
+}
+
+/**
+ * Hex_case_checker - Checks which hex function is calling it
+ * @num: Number to convert into letter
+ * @x: Tells which hex function is calling it
+ * Return: Ascii value for a letter
+ */
+int Hex_case_checker(int num, char x)
+{
+	char *hex = "abcdef";
+	char *Hex = "ABCDEF";
+
+	num = num - 10;
+	if (x == 'x')
+		return (hex[num]);
+	else
+		return (Hex[num]);
+	return (0);
+}
