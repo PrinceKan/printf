fe3557412685c84bd935d11a5a805884f1b8dc28
Moussa KANE <princekan9.mk@gmail.com>
2022-10-21 17:15:48 -0700

    What one has not experienced, one will never understand in print

--- number_printer.c
+++ number_printer.c
@@ -36,3 +36,32 @@ int funct_num(va_list args)
 
 	return (len);
 }
+
+/**
+ * unsignedNumberPrinter - Prints an unsigned number
+ * @n: unsigned integer to be printed
+ * Return: The amount of numbers printed
+ */
+int unsignedNumberPrinter(unsigned int n)
+{
+	int div;
+	int len;
+	unsigned int num;
+
+	div = 1;
+	len = 0;
+
+	num = n;
+
+	for (; num / div > 9; )
+		div *= 10;
+
+	for (; div != 0; )
+	{
+		len += funct_putchar('0' + num / div);
+		num %= div;
+		div /= 10;
+	}
+
+	return (len);
+}
